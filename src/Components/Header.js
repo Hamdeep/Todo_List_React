3d8a6835fe37e92eb4ac84bd0adc6d9c8870052f
Hamdeep <hamdeep0254.be20@chitkara.edu.in>
2022-12-22 18:17:09 +0530

    Basic Commit

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <h1> What's Your Plan Buddy!</h1>
+    </div>
+  )
+}
+
+export default Header
